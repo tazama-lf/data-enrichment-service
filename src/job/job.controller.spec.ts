1b809d72a0ac677a4dfa276c08ae45279b2fbb1a
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-09-16 13:12:31 +0500

    test: Job Tests Updated

--- src/job/job.controller.spec.ts
+++ src/job/job.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JobController } from './job.controller';
+import { JobService } from './job.service';
 
 describe('JobController', () => {
   let controller: JobController;
+  let fakeJobService: Partial<JobService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [JobController],
+      providers: [{ provide: JobService, useValue: fakeJobService }],
     }).compile();
 
     controller = module.get<JobController>(JobController);
--- src/job/job.service.spec.ts
+++ src/job/job.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { JobService } from './job.service';
+import { PrismaService } from '../../prisma/prisma.service';
+import { SchedulerRegistry } from '@nestjs/schedule';
+import { ExecutorService } from '../executor/executor.service';
 
 describe('JobService', () => {
   let service: JobService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [JobService],
+      providers: [JobService, PrismaService, SchedulerRegistry, ExecutorService],
     }).compile();
 
     service = module.get<JobService>(JobService);
