93b96d16966bac8e27b0ff167404f97f4766da63
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-09-16 11:49:40 +0500

    feat: Jobs Fetched

--- src/executor/executor.service.ts
+++ src/executor/executor.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class ExecutorService {
 
     async run(job: any) {
         console.log('Executing job:', job);
+        const jobs = await axios.get(job.sourcePath);
     }
 }
--- src/job/job.service.ts
+++ src/job/job.service.ts
@@ -9,18 +9,27 @@ import { CronJob } from 'cron';
 export class JobService {
     constructor(private prismaService: PrismaService, private schedulerRegistry: SchedulerRegistry, private executorService: ExecutorService) { }
 
+
+    async onModuleInit() {
+        const jobs = await this.findAll();
+        jobs.forEach(job => this.addJob(job as CreateJobDto));
+    }
+
     async create(job: CreateJobDto) {
         const newJob = this.prismaService.job.create({ data: job })
-
-        const cronJob = new CronJob(job.cronExpression, async () => {
-            await this.executorService.run(job);
-        });
-        this.schedulerRegistry.addCronJob(`job`, cronJob as any);
-        cronJob.start();
+        this.addJob(job);
         return newJob;
     }
 
     async findAll() {
         return this.prismaService.job.findMany({})
+    }
+
+    async addJob(job: CreateJobDto) {
+        const cronJob = new CronJob(job.cronExpression, async () => {
+            await this.executorService.run(job);
+        });
+        this.schedulerRegistry.addCronJob(`job-${Math.random().toString()}`, cronJob as any);
+        cronJob.start();
     }
 }
