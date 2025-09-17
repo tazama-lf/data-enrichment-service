ada99e68f0b39961e455a11f42d76c4122e273a8
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-09-17 18:15:57 +0500

    feat: Trigger Job Manually

--- src/job/job.controller.ts
+++ src/job/job.controller.ts
@@ -1,20 +1,27 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateJobDto } from './dto/create-job.dto';
 import { JobService } from './job.service';
 
 @Controller('job')
 export class JobController {
+  constructor(private jobService: JobService) {}
 
-    constructor(private jobService: JobService) {}
+  @Post('/create')
+  async createJob(@Body() job: CreateJobDto) {
+    return this.jobService.create(job);
+  }
 
-    @Post('/create')
-    async createJob(@Body() job : CreateJobDto){
-        return this.jobService.create(job)
-    }
+  @Get('/all')
+  async getAll() {
+    return this.jobService.findAll();
+  }
 
-
-    @Get('/all')
-    async getAll(){
-        return this.jobService.findAll()
-    }
+  @Get('/:id')
+  async triggerJob(@Param('id') id: string) {
+    await this.jobService.runJob(parseInt(id));
+    return {
+      success: true,
+      message: `Job with ID ${id} was triggered successfully`,
+    };
+  }
 }
--- src/job/job.service.ts
+++ src/job/job.service.ts
@@ -1,35 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateJobDto } from './dto/create-job.dto';
 import { PrismaService } from '../../prisma/prisma.service';
 import { SchedulerRegistry } from '@nestjs/schedule';
 import { ExecutorService } from '../executor/executor.service';
 import { CronJob } from 'cron';
+import { Job } from './job.entity';
 
 @Injectable()
 export class JobService {
-    constructor(private prismaService: PrismaService, private schedulerRegistry: SchedulerRegistry, private executorService: ExecutorService) { }
+  constructor(
+    private prismaService: PrismaService,
+    private schedulerRegistry: SchedulerRegistry,
+    private executorService: ExecutorService,
+  ) {}
 
+  async onModuleInit() {
+    const jobs = await this.findAll();
+    jobs.forEach((job) => void this.addJob(job as Job));
+  }
 
-    async onModuleInit() {
-        const jobs = await this.findAll();
-        jobs.forEach(job => this.addJob(job as CreateJobDto));
+  async create(job: CreateJobDto) {
+    const newJob = await this.prismaService.job.create({ data: job });
+    this.addJob(newJob as Job);
+    return newJob;
+  }
+
+  async findAll() {
+    return await this.prismaService.job.findMany({});
+  }
+
+  async findOne(id: number) {
+    const job = await this.prismaService.job.findUnique({ where: { id } });
+    if (!job) {
+      throw new NotFoundException('Job Not Found');
     }
+    return job;
+  }
 
-    async create(job: CreateJobDto) {
-        const newJob = this.prismaService.job.create({ data: job })
-        this.addJob(job);
-        return newJob;
-    }
+  addJob(job: Job) {
+    const cronJob = new CronJob(job.cronExpression, async () => {
+      await this.executorService.run(job);
+    });
+    this.schedulerRegistry.addCronJob(`job-${Math.random().toString()}`, cronJob as any);
+    cronJob.start();
+  }
 
-    async findAll() {
-        return this.prismaService.job.findMany({})
-    }
-
-    async addJob(job: CreateJobDto) {
-        const cronJob = new CronJob(job.cronExpression, async () => {
-            await this.executorService.run(job);
-        });
-        this.schedulerRegistry.addCronJob(`job-${Math.random().toString()}`, cronJob as any);
-        cronJob.start();
-    }
+  async runJob(id: number) {
+    const job = await this.findOne(id);
+    await this.executorService.run(job as Job);
+  }
 }
