473e0d821d78a6cd2b1e38daa96db823cb3ef14e
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-10-07 17:53:55 +0500

    feat: Edited the knefile

--- knex/knexfile.ts
+++ knex/knexfile.ts
@@ -4,10 +4,11 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.CONFIGURATION_DATABASE_HOST || 'localhost',
+      host: process.env.CONFIGURATION_DATABASE_HOST || '10.10.80.37',
       user: process.env.CONFIGURATION_DATABASE_USER || 'postgres',
       password: process.env.CONFIGURATION_DATABASE_PASSWORD || 'postgres',
-      database: process.env.CONFIGURATION_DATABASE || 'mydb',
+      database: process.env.CONFIGURATION_DATABASE || 'tcs',
+      port: 5432,
     },
     migrations: {
       directory: './migrations',
