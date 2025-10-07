3ae2d322552008740a2c96eee82641e3900d7e68
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-10-07 17:59:49 +0500

    feat: Job Mode Update Configured

--- src/job/dto/update-status.dto.ts
+++ src/job/dto/update-status.dto.ts
@@ -1,9 +1,13 @@
 import { IsEnum } from 'class-validator';
-import { JobStatus } from '../../utils/interfaces';
+import { IngestMode, JobStatus } from '../../utils/interfaces';
 import { Expose } from 'class-transformer';
 
 export class UpdateJobStatusDto {
   @Expose()
   @IsEnum(JobStatus)
   job_status: JobStatus;
+
+  @Expose()
+  @IsEnum(IngestMode)
+  mode: IngestMode;
 }
