63cb788e78f7e36d64a71b745403029f6f935d40
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-10-08 19:27:26 +0500

    feat: Update Mode removed of job

--- src/job/dto/update-status.dto.ts
+++ src/job/dto/update-status.dto.ts
@@ -1,13 +1,9 @@
-import { IsEnum } from 'class-validator';
-import { IngestMode, JobStatus } from '../../utils/interfaces';
 import { Expose } from 'class-transformer';
+import { IsEnum } from 'class-validator';
+import { JobStatus } from '../../utils/interfaces';
 
 export class UpdateJobStatusDto {
   @Expose()
   @IsEnum(JobStatus)
   job_status: JobStatus;
-
-  @Expose()
-  @IsEnum(IngestMode)
-  mode: IngestMode;
 }
