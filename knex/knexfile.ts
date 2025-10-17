0275f4ba0347638e82fa0ef9040c82e8cfce316d
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-10-17 16:55:52 +0500

    fix: Migrations Fixed

--- knex/knexfile.ts
+++ knex/knexfile.ts
@@ -4,10 +4,10 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.CONFIGURATION_DATABASE_HOST || '10.10.80.37',
+      host: process.env.CONFIGURATION_DATABASE_HOST || 'localhost',
       user: process.env.CONFIGURATION_DATABASE_USER || 'postgres',
       password: process.env.CONFIGURATION_DATABASE_PASSWORD || 'postgres',
-      database: process.env.CONFIGURATION_DATABASE || 'tcs',
+      database: process.env.CONFIGURATION_DATABASE || 'mydb',
       port: 5432,
     },
     migrations: {
--- knex/migrations/20250919094911_create_job_table.ts
+++ knex/migrations/20250919094911_create_job_table.ts
@@ -2,15 +2,15 @@ import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('job', (table) => {
-    table.increments('id').primary();
-    table.enu('config_type', ['Pull', 'Push']).notNullable();
+    table.string('id').notNullable().primary();
     table.string('endpoint_name').notNullable();
     table.enu('source_type', ['SFTP', 'HTTP']).notNullable();
-    table.string('description').notNullable();
+    table.string('de  scription').notNullable();
     table.jsonb('connection').notNullable();
     table.jsonb('file');
     table.string('table_name').notNullable();
-    table.enu('job_status', ['PENDING', 'IN-PROGRESS', 'SUSPENDED', 'CLONED']).notNullable();
+    table.enu('mode', ['append', 'replace']).notNullable().defaultTo('append');
+    table.enu('job_status', ['PENDING', 'IN-PROGRESS', 'SUSPENDED', 'CLONED']).notNullable().defaultTo('PENDING');
     table.timestamps(true, true);
   });
 }
