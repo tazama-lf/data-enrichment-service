1d4791eec0600c0690679245a195c6e64ec7885a
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-11-06 20:29:46 +0500

    refactor: update tenant_id check if mismatched tenant_id

--- src/job/job.service.ts
+++ src/job/job.service.ts
@@ -95,7 +95,7 @@ export class JobService implements OnModuleInit {
 
       if (cachedEndpoint) {
         endpoint = JSON.parse(cachedEndpoint);
-        if (endpoint && endpoint.tenant_id === tenantId) {
+        if (endpoint && endpoint.tenant_id !== tenantId) {
           throw new NotFoundException(`Endpoint '${path}' does not exist with tenant_id ${tenantId}`);
         }
         this.loggerService.log(`Using endpoint from cache: ${path}`);
