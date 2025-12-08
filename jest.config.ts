0f910450b3aa83dcb91e314bcd1774857c4da27e
Muhammad Ali <ali.shoaib@paysyslabs.com>
2025-12-08 13:41:48 +0500

    fix: update coverage settings and generated files in .gitignore and jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from '@jest/types';
 
-// Jest config for NestJS project with tests inside /test folder
 const config: Config.InitialOptions = {
   bail: 1,
   clearMocks: true,
@@ -10,30 +9,22 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
 
-  // 👇 IMPORTANT: You store tests inside /test folder
   roots: ['<rootDir>/test'],
 
-  // Use spec or test convention
   testMatch: ['**/*.spec.ts', '**/*.test.ts'],
 
-  // Transform TS using ts-jest
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
 
-  // Load .env before all tests
   setupFiles: ['dotenv/config'],
 
-  // Only if you create this file (optional):
-  // setupFilesAfterEnv: ['<rootDir>/src/config/jest.setup.ts'],
-
-  // Coverage settings
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts', '!src/main.ts', '!src/**/index.ts', '!src/**/*.module.ts'],
   coverageDirectory: '<rootDir>/coverage',
   coverageProvider: 'v8',
 
-  coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/test/', '.mock.ts', '.module.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/test/', '.mock.ts', '.module.ts', '.*utils.*', '.*validators.*', '.*dto.*'],
 
   verbose: true,
 };
